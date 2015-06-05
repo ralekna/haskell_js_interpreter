221730377bbc7411d24fe28ac6e9ef27e2beff4f
Rytis Alekna <r.alekna@gmail.com>
2015-06-05 18:01:15 +0300

    fixed compilation

--- src/Main.hs
+++ src/Main.hs
@@ -7,5 +7,5 @@ import Calculator(addOne, third, takeOne)
 
 main = do
   print( addOne(2) )
-  print( third(('q','w','e')) )
-  print( takeOne([7,8,9], 2) )
+  print( (third ('q','w','e') ) )
+  print( takeOne [7,8,9] 2 )
