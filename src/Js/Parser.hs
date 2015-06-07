66fee91e570ff87a095d2f3b94e67b58c6bfec3c
Rytis Alekna <r.alekna@gmail.com>
2015-06-07 09:42:21 +0300

    string spliter unfinished

--- src/Js/Parser.hs
+++ src/Js/Parser.hs
@@ -1,16 +1,18 @@
 module Js.Parser(
  reverseList
+,tokenizeString
 ) where
 
-{-
+
 tokenizeString :: (String a) => a -> [a]
 -- tokenizeString "" = [""]
-tokenizeString string@x:xs memo
- | memo == [] = tokenizeString xs
- | string == [] = memo
+tokenizeString string@(x:xs) memo
+ | string == [] = []
+ | memo == [] = reverseList xs
+ | xs == [] = memo
  | x == ' ' = memo:(tokenizeString xs)
  | otherwise =
--}
+
 
 reverseList :: (Eq a) => [a] -> [a]
 reverseList list@(x:xs)
